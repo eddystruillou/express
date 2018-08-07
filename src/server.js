3353582a45d6a4981c6ac42dc0353261472e8e3d
Eddy Struillou <eddy.s25@hotmail.fr>
2018-08-07 15:13:31 +0200

    Mise en place de la requête

--- src/server.js
+++ src/server.js
@@ -2,18 +2,29 @@
 import express from 'express'
 const app = express();
 
-app.get('/Ping', (req, res) => {
+app.get('/Et', (req, res) => {
   res
   .status(200)
   .setHeader('content-type', 'text/html')
-  res.send('Pong');
+  res.send('Mercéééééééééééééééé');
 });
 
-app.get('/Movies', (req, res) => {
-  const fs = require("fs")
-  let rawdata = fs.readFileSync('./movies.json')
-  let movies = JSON.parse(rawdata)
-  res.send(movies)
-})
+app.use((req, res, next) => {
+  setTimeout(next, 3000)
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8081')
+  next()
+});
+
+app.get('/Movies', 
+  (req, res) => {
+    const fs = require("fs")
+    let rawdata = fs.readFileSync('./movies.json')
+    let movies = JSON.parse(rawdata)
+    res.send(movies)
+  }
+);
 
 app.listen(5000, () => console.log('started'));
